9e1129f308c032c93fc4e7a24ca816a6ed2e6f08
Ruchi <ruchi9n02@gmail.com>
2022-12-03 00:59:22 +0530

    aruco file

--- test.py
+++ test.py
@@ -1,50 +1,97 @@
-import cv2
 import numpy as np
-  
-# Reading an image
-img = cv2.imread('image.png')
-  
-# The kernel to be used for dilation purpose
-kernel = np.ones((5, 5), np.uint8)
-  
-# converting the image to HSV format
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-  
-# defining the lower and upper values of HSV,
-# yellow colour
-Lower_hsv = np.array([20, 70, 100])
-Upper_hsv = np.array([50, 255, 255])
-#blue
-Lower_hsv = np.array([90, 50, 70])
-Upper_hsv = np.array([128, 255, 255])
-#red
-Lower_hsv = np.array( [0, 50, 70])
-Upper_hsv = np.array([9, 255, 255])
-#green
-Lower_hsv = np.array([36, 50, 70])
-Upper_hsv = np.array([89, 255, 255])
-# #pink
-Lower_hsv = np.array( [159, 50, 70])
-Upper_hsv = np.array([180, 255, 255])
-  
-# creating the mask by eroding,morphing,
-# dilating process
-Mask = cv2.inRange(hsv, Lower_hsv, Upper_hsv)
-Mask = cv2.erode(Mask, kernel, iterations=1)
-Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, kernel)
-Mask = cv2.dilate(Mask, kernel, iterations=1)
-  
-# Inverting the mask by
-# performing bitwise-not operation
-# Mask = cv2.bitwise_not(Mask)
-  
-# Displaying the image
-cv2.imshow('Mask', Mask)
-  
-# waits for user to press any key
-# (this is necessary to avoid Python
-# kernel form crashing)
-cv2.waitKey(0)
-  
-# closing all open windows
+import time
+import cv2
+
+
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+def aruco_display(corners, ids, rejected, image):
+	if len(corners) > 0:
+		
+		ids = ids.flatten()
+		
+		for (markerCorner, markerID) in zip(corners, ids):
+			
+			corners = markerCorner.reshape((4, 2))
+			(topLeft, topRight, bottomRight, bottomLeft) = corners
+			
+			topRight = (int(topRight[0]), int(topRight[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+			cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+			cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+			cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+			cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+			
+			cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+			cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+			
+			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+				0.5, (0, 255, 0), 2)
+			print("[Inference] ArUco marker ID: {}".format(markerID))
+			
+	return image
+
+
+
+
+aruco_type = "DICT_ARUCO_ORIGINAL"
+
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+
+cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
+while cap.isOpened():
+    
+	ret, img = cap.read()
+
+	h, w, _ = img.shape
+
+	width = 1000
+	height = int(width*(h/w))
+	img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+ 
+	corners, ids, rejected = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
+
+	detected_markers = aruco_display(corners, ids, rejected, img)
+
+	cv2.imshow("Image", detected_markers)
+
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+	    break
 cv2.destroyAllWindows()
+cap.release()
